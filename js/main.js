ac84a6df9d20da7b4ef24e780f62d42752aaf15a
mostafamohamady <mostafa.mohamady.2000@gmail.com>
2021-01-18 16:40:47 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,38 +1,21 @@
-var swiper = new Swiper(".swiper-container--coll", {
+// main slider
+var swiper = new Swiper(".swiper-container--index", {
   spaceBetween: 0,
-  centeredSlides: true,
-  loop: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-});
-
-var swiper = new Swiper(".swiper-container--management", {
-  slidesPerView: 3,
-  spaceBetween: 10,
-  slidesPerGroup: 1,
   loop: true,
   loopFillGroupWithBlank: true,
+  speed: 700,
   autoplay: {
-    delay: 2500,
+    delay: 6000,
     disableOnInteraction: false,
   },
-  breakpoints: {
-    992: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 1,
-     
-    },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
+// // main slaider
