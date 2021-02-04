ef84ad9f9c0a18d915a2175da6595f405ab725d6
mostafamohamady <mostafa.mohamady.2000@gmail.com>
2021-02-04 21:39:09 +0200

    update

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var stickyNav = (function () {
   var navbar = document.getElementById("navigation");
   body = document.getElementsByTagName("body")[0];
   // Get the offset position of the navbar
-  var sticky = 100;
+  var sticky = 50;
   // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
   function myFunction() {
     if (window.pageYOffset >= sticky) {
